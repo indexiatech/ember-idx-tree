108caa0ad52bc8d8b8ecd01c0a88f7ec519deeb3
beatle <kudgo.test@gmail.com>
2015-01-14 00:36:21 +0200

    Move broccoli staff to Brocfile

--- index.js
+++ index.js
@@ -1,40 +1,6 @@
 /* jshint node: true */
 'use strict';
-var mergeTrees = require( 'broccoli-merge-trees' ),
-    pickFiles  = require( 'broccoli-static-compiler' );
 
 module.exports = {
-    name: 'ember-idx-tree',
-
-    included: function( app ) {
-        this._super.included( app );
-        var isDummy = app.name === 'dummy'
-
-        if (isDummy) {
-            app.import( 'bower_components/bootstrap/dist/css/bootstrap-theme.css.map' );
-
-            app.import({
-                development : 'bower_components/bootstrap/dist/css/bootstrap.css'
-            });
-            app.import({
-                development : 'bower_components/fontawesome/css/font-awesome.min.css'
-            });
-            app.import({
-                development : 'bower_components/highlightjs/highlight.pack.js'
-            });
-            app.import({
-                development : 'bower_components/highlightjs/styles/tomorrow.css'
-            });
-        }
-    },
-
-    postprocessTree: function( type, tree ) {
-        return mergeTrees([ tree,
-            pickFiles( 'bower_components/fontawesome/fonts', {
-                srcDir  : '/',
-                files   : [ 'fontawesome-webfont.woff' ],
-                destDir : '/fonts'
-            })
-        ]);
-    },
+    name: 'ember-idx-tree'
 };
